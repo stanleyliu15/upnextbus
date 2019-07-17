4496c5ef98b60351682958bc9f7da70ca47bc461
Stanley <Stanleyliu15@gmail.com>
2019-07-17 00:13:09 -0700

    setup eslint, prettier, husky with typescript
    eslintrc setup may change depending on future needs
    eslintrc setup so prettier will override all other linters for formatting issues
    
    setup husky + lint-staged for ensuring bad code doesnt slip through
    
    added dependencies for eslint for typescriot
    "@typescript-eslint/eslint-plugin": "^1.12.0",
    "@typescript-eslint/parser": "^1.12.0",
    
    added eslint dependencies based on airbnb's style guide:
    "eslint": "5.3.0",
    "eslint-config-airbnb": "17.1.1",
    "eslint-plugin-import": "^2.18.0",
    "eslint-plugin-jsx-a11y": "^6.2.3",
    "eslint-plugin-react": "^7.14.2",
    
    added prettier as a linter for main formatter of code
    "eslint-config-prettier": "^6.0.0",
    "eslint-plugin-prettier": "^3.1.0",
    "prettier": "^1.18.2",
    
    added additional helpful linters:
    "eslint-plugin-react-hooks": "^1.6.1",
    "eslint-plugin-react-native": "^3.7.0",
    "eslint-plugin-jest": "^22.9.0",

---
+++ .eslintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+  env: {
+    node: true,
+    browser: true,
+    es6: true,
+    "jest/globals": true, // whitelist the env variables provided by Jest
+    "react-native/react-native": true // whitelist all browser-like globals
+  },
+  parser: "@typescript-eslint/parser", // custom eslint parser that allows eslint to lint ts code
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  plugins: ["react", "react-native", "react-hooks", "@typescript-eslint", "jest", "prettier"],
+  extends: [
+    "airbnb",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    "plugin:jest/recommended",
+    "plugin:@typescript-eslint/recommended",
+    /* use prettier as the main code formatter so anything pretty should always be last */
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended",
+    "prettier/react"
+  ],
+  rules: {
+    /* React */
+    "react/jsx-filename-extension": [1, { extensions: [".jsx", ".tsx"] }],
+    /* React Hooks */
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    /* React Native */
+    "react-native/no-unused-styles": 2, // detect unused stylesheet rules
+    "react-native/split-platform-components": 0, // enforce platform specific filenames
+    "react-native/no-inline-styles": 2, // detect inline styles with literal values
+    "react-native/no-color-literals": 2, // detect stylesheet colors using literals
+    "react-native/no-raw-text": 2, // detech raw text outside of <Text>,
+    /* TypeScript */
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    /* Imports */
+    "import/prefer-default-export": "off",
+    /* JavaScript */
+    "func-names": "off"
+  }
+};
