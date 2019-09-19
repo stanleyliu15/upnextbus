7d9df1938add1c3cafd2050c21e901cf818be056
Stanley <Stanleyliu15@gmail.com>
2019-09-18 22:03:32 -0700

    fix: reset error message on each new api request

--- src/store/features/nextbus/reducer.ts
+++ src/store/features/nextbus/reducer.ts
@@ -15,9 +15,13 @@ const reducer = combineReducers<NextBusState>({
     data: createReducer([]).handleAction(actions.getAgenciesAsync.success, (state, action) => {
       return action.payload;
     }),
-    error: createReducer(null).handleAction(actions.getAgenciesAsync.failure, (state, action) => {
-      return action.payload;
-    })
+    error: createReducer(null)
+      .handleAction(actions.getAgenciesAsync.failure, (state, action) => {
+        return action.payload;
+      })
+      .handleAction(actions.getAgenciesAsync.request, (state, action) => {
+        return null;
+      })
   }),
   routes: combineReducers({
     loading: createReducer(false as boolean)
@@ -29,9 +33,13 @@ const reducer = combineReducers<NextBusState>({
     data: createReducer([]).handleAction(actions.getRoutesAsync.success, (state, action) => {
       return action.payload;
     }),
-    error: createReducer(null).handleAction(actions.getRoutesAsync.failure, (state, action) => {
-      return action.payload;
-    })
+    error: createReducer(null)
+      .handleAction(actions.getRoutesAsync.failure, (state, action) => {
+        return action.payload;
+      })
+      .handleAction(actions.getRoutesAsync.request, (state, action) => {
+        return null;
+      })
   }),
   predictions: combineReducers({
     loading: createReducer(false as boolean)
@@ -43,12 +51,13 @@ const reducer = combineReducers<NextBusState>({
     data: createReducer(null).handleAction(actions.getPredictionsAsync.success, (state, action) => {
       return action.payload;
     }),
-    error: createReducer(null).handleAction(
-      actions.getPredictionsAsync.failure,
-      (state, action) => {
+    error: createReducer(null)
+      .handleAction(actions.getPredictionsAsync.failure, (state, action) => {
         return action.payload;
-      }
-    )
+      })
+      .handleAction(actions.getPredictionsAsync.request, (state, action) => {
+        return null;
+      })
   }),
   nearbyPredictionsList: combineReducers({
     loading: createReducer(false as boolean)
@@ -66,12 +75,13 @@ const reducer = combineReducers<NextBusState>({
         return action.payload;
       }
     ),
-    error: createReducer(null).handleAction(
-      actions.getNearbyPredictionListAsync.failure,
-      (state, action) => {
+    error: createReducer(null)
+      .handleAction(actions.getNearbyPredictionListAsync.failure, (state, action) => {
         return action.payload;
-      }
-    )
+      })
+      .handleAction(actions.getNearbyPredictionListAsync.request, (state, action) => {
+        return null;
+      })
   }),
   nearestAgencyIdByLocation: combineReducers({
     loading: createReducer(false as boolean)
@@ -89,12 +99,13 @@ const reducer = combineReducers<NextBusState>({
         return action.payload;
       }
     ),
-    error: createReducer(null).handleAction(
-      actions.getNearestAgencyIdByLocationAsync.failure,
-      (state, action) => {
+    error: createReducer(null)
+      .handleAction(actions.getNearestAgencyIdByLocationAsync.failure, (state, action) => {
         return action.payload;
-      }
-    )
+      })
+      .handleAction(actions.getNearestAgencyIdByLocationAsync.request, (state, action) => {
+        return null;
+      })
   }),
   selectedAgencyId: createReducer(null).handleAction(actions.selectAgencyId, (state, action) => {
     return action.payload;
