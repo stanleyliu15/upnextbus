9654016feb2731596a6cdab52123ddca6654cdc5
Stanley <Stanleyliu15@gmail.com>
2020-08-24 17:22:57 -0700

    (fix) rename project (add files back)
    ios files were lowercase on git

---
+++ ios/Upnextbus/AppDelegate.h
@@ -0,0 +1,11 @@
+#import <UIKit/UIKit.h>
+#import <UMReactNativeAdapter/UMModuleRegistryAdapter.h>
+#import <React/RCTBridgeDelegate.h>
+#import <UMCore/UMAppDelegateWrapper.h>
+
+@interface AppDelegate : UMAppDelegateWrapper <RCTBridgeDelegate>
+
+@property (nonatomic, strong) UMModuleRegistryAdapter *moduleRegistryAdapter;
+@property (nonatomic, strong) UIWindow *window;
+
+@end
